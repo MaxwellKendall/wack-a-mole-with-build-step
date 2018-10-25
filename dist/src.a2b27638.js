0663dbabef844c67cd3638f635d7b9f9f89831d9
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2018-10-24 23:42:01 -0400

    troubleshooting style issue with ghpages

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -171,7 +171,7 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"src/styles/index.scss":[function(require,module,exports) {
+},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"src/styles/scss/index.scss":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
@@ -179,7 +179,7 @@ module.hot.accept(reloadCSS);
 },{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"src/index.js":[function(require,module,exports) {
 "use strict";
 
-require("./styles/index.scss");
+require("./styles/scss/index.scss");
 
 document.querySelector('.start').addEventListener('click', function () {
   console.log('add start function');
@@ -190,7 +190,7 @@ document.querySelector(".stop").addEventListener("click", function () {
 document.querySelector(".reset").addEventListener("click", function () {
   console.log("add reset function");
 });
-},{"./styles/index.scss":"src/styles/index.scss"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./styles/scss/index.scss":"src/styles/scss/index.scss"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54128" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60603" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
