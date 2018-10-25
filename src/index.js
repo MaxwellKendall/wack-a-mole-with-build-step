f753e8494de02cbf6962c683d3db200cebe5c31f
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2018-10-24 23:07:49 -0400

    setting up event listeners

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import './styles/index.scss';
-import Footer from './components/Footer';
 
-const renderApp = () => {
-  return `<h1 class="test">Hello Parcel!</h1>
-            <div>
-              Look
-              <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">right nea baby</a>
-              for more info about Parcel.
-            </div>
-  `;
-}
+document.querySelector('.start').addEventListener('click', () => {
+  console.log('add start function')
+});
 
-document.getElementById("app").innerHTML = renderApp();
+document.querySelector(".stop").addEventListener("click", () => {
+  console.log("add stop function");
+});
+
+document.querySelector(".reset").addEventListener("click", () => {
+  console.log("add reset function");
+});
