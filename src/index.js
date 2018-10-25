84390b36139cb24a8fca39cbca2f076b8201e87c
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2018-10-24 22:00:00 -0400

    build config all set up

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import './styles/index.scss';
-document.getElementById("app").innerHTML = `
-<h1 class="test">Hello Parcel!</h1>
-<div>
-  Look
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Parcel.
-</div>
-`;
+import Footer from './components/Footer';
+
+const renderApp = () => {
+  return `<h1 class="test">Hello Parcel!</h1>
+            <div>
+              Look
+              <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">right nea baby</a>
+              for more info about Parcel.
+            </div>
+  `;
+}
+
+document.getElementById("app").innerHTML = renderApp();
