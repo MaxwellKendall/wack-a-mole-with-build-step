1e57141c235856625719baaf8b4047501ced3d3b
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2018-10-25 18:09:12 -0400

    WIP - adjusting logic to end gameClock and activeTimeouts

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import './styles/scss/index.scss';
 
 // global variables
 let activeTimeout;
+let gameClock;
+let isGameOver = false;
+let score = 0;
+let numberOfHogs = 0;
 
+// utils
 const generateRandomNumber = () => Math.floor(Math.random() * 3000);
 const generateRandomNumberBetween1and9 = () => Math.floor(Math.random() * 9) + 1; // random * high range of 10 minus 1, plus low range of 1
+
+// game controls
 const endGame = (message = 'Game over!') => {
+  isGameOver = true;
   clearTimeout(activeTimeout);
-  activeTimeout = message;
+  clearTimeout(gameClock);
   document.querySelectorAll('.hole')
     .forEach((hole) => {
       hole.children[0].classList.add('hide');
@@ -28,15 +36,19 @@ const hideHogAfterRandomDelay = (hog) => {
 }
 
 const showRandomGroundHog = () => {
-  const randomHog = document.querySelector(`.hole-${generateRandomNumberBetween1and9()}`).children[0];
-  randomHog.classList.remove('hide');
-  hideHogAfterRandomDelay(randomHog);
+  if (!isGameOver) {
+    const randomHog = document.querySelector(`.hole-${generateRandomNumberBetween1and9()}`).children[0];
+    randomHog.classList.remove('hide');
+    numberOfHogs ++;
+    hideHogAfterRandomDelay(randomHog);
+  }
 }
 
 // Event listeners
 document.querySelector('.start').addEventListener('click', () => {
+  isGameOver = false;
   showRandomGroundHog();
-  window.setTimeout(endGame, 15000);
+  gameClock = window.setTimeout(endGame, 15000);
 });
 
 document.querySelector(".stop").addEventListener("click", () => {
@@ -48,3 +60,15 @@ document.querySelector(".reset").addEventListener("click", () => {
   console.log("add reset function");
   resetGame();
 });
+
+document.querySelectorAll('img').forEach((hog) => {
+  hog.addEventListener('click', () => {
+    console.log('hog was clicked');
+    hog.classList.add('hide');
+    score ++;
+  })
+})
+
+const displayScore = () => {
+  document.getElementById("score").innerHTML = `SCORE: ${score} out of ${numberOfHogs}`;
+}
