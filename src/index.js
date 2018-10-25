0d2ff72a48a8174a90e0ab4dc1a817aa774504b8
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2018-10-24 21:15:22 -0400

    init with parcel bundler

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './styles/index.scss';
+document.getElementById("app").innerHTML = `
+<h1 class="test">Hello Parcel!</h1>
+<div>
+  Look
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
+  for more info about Parcel.
+</div>
+`;
