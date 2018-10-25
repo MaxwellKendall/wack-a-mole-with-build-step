1506f1f57eb7f803665ce57c632c21b316ee2855
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2018-10-25 09:03:13 -0400

    Logic in place for starting, resetting, stopping game

--- src/index.js
+++ src/index.js
@@ -1,35 +1,50 @@
 import './styles/scss/index.scss';
 
-const generateRandomNumber = () => Math.floor(Math.random() * 10000);
+// global variables
+let activeTimeout;
+
+const generateRandomNumber = () => Math.floor(Math.random() * 3000);
 const generateRandomNumberBetween1and9 = () => Math.floor(Math.random() * 9) + 1; // random * high range of 10 minus 1, plus low range of 1
-const endGame = () => {
-  setTimeout(() => {
-    /** 
-     * After some amount of time, hide all groundhogs and set a flag which prevents showRandomGroundHog from triggering
-    */
-  })
+const endGame = (message = 'Game over!') => {
+  clearTimeout(activeTimeout);
+  activeTimeout = message;
+  document.querySelectorAll('.hole')
+    .forEach((hole) => {
+      hole.children[0].classList.add('hide');
+    });
+  window.alert(message);
+}
+
+const resetGame = () => {
+  endGame('resetting game!');
+  showRandomGroundHog();
 }
 
 const hideHogAfterRandomDelay = (hog) => {
-  setTimeout(() => {
-    hog.classList.remove('show').add('hide');
+  activeTimeout = setTimeout(() => {
+    hog.classList.add('hide');
     showRandomGroundHog();
   }, generateRandomNumber())
 }
+
 const showRandomGroundHog = () => {
-  const randomHog = document.querySelector(`.hole-${generateRandomNumberBetween1and9()}`);
-  randomHog.classList.remove('hide').add('show');
+  const randomHog = document.querySelector(`.hole-${generateRandomNumberBetween1and9()}`).children[0];
+  randomHog.classList.remove('hide');
   hideHogAfterRandomDelay(randomHog);
 }
+
+// Event listeners
 document.querySelector('.start').addEventListener('click', () => {
-  console.log('yoooooo');
   showRandomGroundHog();
+  window.setTimeout(endGame, 15000);
 });
 
 document.querySelector(".stop").addEventListener("click", () => {
   console.log("add stop function");
+  endGame('Ending game...');
 });
 
 document.querySelector(".reset").addEventListener("click", () => {
   console.log("add reset function");
+  resetGame();
 });
