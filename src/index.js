28e96fdb16e20469ce7be19115dea77315ff329d
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2018-10-25 00:31:02 -0400

    WIP -- beginning to set up logic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import './styles/scss/index.scss';
 
+const generateRandomNumber = () => Math.floor(Math.random() * 10000);
+const generateRandomNumberBetween1and9 = () => Math.floor(Math.random() * 9) + 1; // random * high range of 10 minus 1, plus low range of 1
+const endGame = () => {
+  setTimeout(() => {
+    /** 
+     * After some amount of time, hide all groundhogs and set a flag which prevents showRandomGroundHog from triggering
+    */
+  })
+}
+
+const hideHogAfterRandomDelay = (hog) => {
+  setTimeout(() => {
+    hog.classList.remove('show').add('hide');
+    showRandomGroundHog();
+  }, generateRandomNumber())
+}
+const showRandomGroundHog = () => {
+  const randomHog = document.querySelector(`.hole-${generateRandomNumberBetween1and9()}`);
+  randomHog.classList.remove('hide').add('show');
+  hideHogAfterRandomDelay(randomHog);
+}
 document.querySelector('.start').addEventListener('click', () => {
-  console.log('add start function')
+  console.log('yoooooo');
+  showRandomGroundHog();
 });
 
 document.querySelector(".stop").addEventListener("click", () => {
